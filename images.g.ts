b546b1000871c7eddac4035c45e24a5f842f5ba2
CodeNinjasBromley <125496244+CodeNinjasBromley@users.noreply.github.com>
2023-03-20 17:08:54 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . c c . c c . . . 
+. . . . . . c c c 3 c c 3 f . . 
+. . . . . c c c b 3 c b 3 c f . 
+. . . . f f b b b b b b b b c f 
+. . . . f f b b b 1 b b b 1 c c 
+. . . f f f c b b b b b b b b c 
+. . . f f f f b b c 1 f f 1 b c 
+. . . b b b c c b f 1 f f 1 f f 
+. . . c c c c f b f f f f f f f 
+. . c c c b b f b f 2 2 2 2 f f 
+. . . c b b c c b 2 2 2 2 2 f . 
+. . c b b c c f f b 2 2 2 f . . 
+. c c c c c f f f f f f f . . . 
+c c c c . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
